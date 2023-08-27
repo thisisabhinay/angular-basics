f609da96843a0435f0a5a3ae40a12636dae11ae8
thisisabhinay <a8h1nay@gmail.com>
2023-08-27 23:56:33 +0530

    build: integrate tailwind and re-write style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core"
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  selector: "app-root",
+  template: `
+    <main class="bg-white p-10 max-w-screen-md mt-10 mx-auto shadow-lg">
+      <h1 class="text-3xl text-gray-300 font-semibold mb-4">Habits List</h1>
+      <app-habit-list></app-habit-list>
+    </main>
+  `,
 })
 export class AppComponent {
-  title = 'angular-basics';
+  title = "angular-basics"
 }
--- src/app/habit-item/habit-item.component.ts
+++ src/app/habit-item/habit-item.component.ts
@@ -2,7 +2,11 @@ import { Component, Input, OnInit } from "@angular/core"
 
 @Component({
   selector: "app-habit-item",
-  template: ` <li>{{ habit.title }}</li>`,
+  template: `<li
+    class="text-gray-700 my-2 hover:text-gray-900 cursor-pointer ease-in-out"
+  >
+    {{ habit.title }}
+  </li>`,
   styles: [],
 })
 export class HabitItemComponent implements OnInit {
